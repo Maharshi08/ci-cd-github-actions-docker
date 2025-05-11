9be3d76ef0759b4f39785e553bd57c19838f3edc
Maharshi08 <maharshi8m86@gmail.com>
2025-05-11 10:34:30 +0000

    Fix: Moved BrowserRouter to index.js for GitHub Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import CountryList from './pages/CountryList';
 import CountryDetail from './pages/CountryDetail';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import Navbar from './components/Navbar'; // ✅ Import your Navbar
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Navbar /> {/* ✅ Use your custom Navbar here */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/countries" element={<CountryList />} />
-          <Route path="/countries/:code" element={<CountryDetail />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/countries" element={<CountryList />} />
+        <Route path="/countries/:code" element={<CountryDetail />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
